b3b385556b8ee4d223661e9dea0a4109f7c0101b
JUAUNNN <joaovitor061jv@gmail.com>
2025-03-26 20:52:55 -0300

    fist commit

---
+++ pedido-venda/src/main/java/com/patterns/pedido_venda/PedidoVendaApplication.java
@@ -0,0 +1,13 @@
+package com.patterns.pedido_venda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoVendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoVendaApplication.class, args);
+	}
+
+}
---
+++ pedido-venda/src/test/java/com/patterns/pedido_venda/PedidoVendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patterns.pedido_venda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoVendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
